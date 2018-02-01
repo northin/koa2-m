426131894d24f119b7eda43215023d8b4a45db54
northin <1477158484@qq.com>
2018-02-01 19:51:54 +0800

    添加 日评论数的折线图的接口

--- server/router/api.js
+++ server/router/api.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const userInfoController = require('./../controller/user_info')
 const projectTopController = require('./../controller/project_top')
+const projectTimeController = require('./../controller/project_time')
 const checkToken = require('../token/checkToken.js');
 
 const routers = router
@@ -9,6 +10,7 @@ const routers = router
 	.post('/user/signIn.json',userInfoController.signIn)
 	.post('/user/reg.json', userInfoController.register)
 	.post('/project/top10.json' ,checkToken ,projectTopController.getTop10)
+	.post('/project/time.json' ,checkToken ,projectTimeController.getTime)
 	// .get('/user/signOut.json',userInfoController.signOut)
 //api/user/signIn.json
 
